6a7195e3a27d04c0038c90497d2b0873510ade56
geno9889 <geno9889@gmail.com>
2021-11-25 10:00:15 +0100

    Package structure fix

---
+++ CalculatorApplication/src/it/unisa/diem/se/calculatorapplication/service/serviceimpl/MathematicalOperations.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package it.unisa.diem.se.calculatorapplication.service.serviceimpl;
+
+/**
+ *
+ * @author Giuseppe
+ */
+public class MathematicalOperations {
+    
+}
