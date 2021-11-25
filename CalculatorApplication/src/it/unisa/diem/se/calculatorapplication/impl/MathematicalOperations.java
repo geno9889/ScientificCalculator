9e0d94e7dec54b3786d564eb371ade782ef36fa4
marcograziano <>
2021-11-25 12:55:35 +0100

    Created MathematicalOperations and SingleOperationInterface with tests

---
+++ CalculatorApplication/src/it/unisa/diem/se/calculatorapplication/impl/MathematicalOperations.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package it.unisa.diem.se.calculatorapplication.impl;
+
+import java.util.List;
+import it.unisa.diem.se.calculatorapplication.entity.ComplexNumber;
+import java.util.ArrayList;
+import java.util.Stack;
+
+/**
+ *
+ * @author Giuseppe
+ */
+public class MathematicalOperations implements SingleOperationsInterface{
+    
+    private ArrayList<String> operations = new ArrayList();
+
+    public MathematicalOperations() {
+        this.operations.add("+");
+        this.operations.add("-");
+        this.operations.add("*");
+        this.operations.add("/");
+    }
+
+    public ArrayList<String> getOperations() {
+        return operations;
+    }
+
+    @Override
+    public boolean executeifExists(String operation, Stack stackNumbers) {
+        if (operation.length()==1){
+            if (operation.contentEquals(operations.get(0))){
+               // ComplexNumbers.sum(stackNumbers.elementAt(1),stackNumbers.elementAt(0));
+                return true;
+            }
+            else if(operation.contentEquals(operations.get(1))){
+               // ComplexNumbers.substraction(stackNumbers.elementAt(1),stackNumbers.elementAt(0));
+                return true;
+            }
+            else if(operation.contentEquals(operations.get(2))){
+               // ComplexNumbers.multiplication(stackNumbers.elementAt(1),stackNumbers.elementAt(0));
+                return true;
+            }
+            
+        }
+        return false;
+    }
+    
+    
+}
+
+
+
---
+++ CalculatorApplication/test/it/unisa/diem/se/calculatorapplicationtest/CalculatorApplicationTest.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package it.unisa.diem.se.calculatorapplicationtest;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author ASUS
+ */
+public class CalculatorApplicationTest {
+    
+    public CalculatorApplicationTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
