1022ffd98c3e01796083bde50570e1c943da0842
Francesco.ferraioli10@gmail.com <>
2021-11-25 10:20:59 +0100

    Calculator Application Test

--- CalculatorApplication/test/it/unisa/diem/se/calculatorapplication/CalculatorApplicationTest.java
+++ CalculatorApplication/test/it/unisa/diem/se/calculatorapplication/CalculatorApplicationTest.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package it.unisa.diem.se.calculatorcontroller.controller;
+package it.unisa.diem.se.calculatorapplication;
 
 /**
  *
  * @author Francesco
  */
-public class CalculatorControllerTest {
+public class CalculatorApplicationTest {
     
 }
