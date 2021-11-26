89538c24f4124d93b3c544de50110e4b33fec45c
geno9889 <ggenovese@sysmanagement.it>
2021-11-26 10:03:14 +0100

    constructor CalculatorController

--- CalculatorApplication/src/it/unisa/diem/se/calculatorapplication/controller/CalculatorController.java
+++ CalculatorApplication/src/it/unisa/diem/se/calculatorapplication/controller/CalculatorController.java
@@ -4,10 +4,39 @@
  */
 package it.unisa.diem.se.calculatorapplication.controller;
 
+import it.unisa.diem.se.calculatorapplication.entity.ComplexNumber;
+import it.unisa.diem.se.calculatorapplication.service.MathematicalOperations;
+import it.unisa.diem.se.calculatorapplication.service.SingleOperationsInterface;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 /**
  *
  * @author Giuseppe
  */
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
 public class CalculatorController {
     
+    private Stack<ComplexNumber> stackNumbers;
+    private List<SingleOperationsInterface> singleOperations;
+    
+    public CalculatorController(){
+        stackNumbers = new Stack<>();
+        singleOperations = new LinkedList<>();
+        singleOperations.add(new MathematicalOperations());
+    }
+    
+    public Stack insertOrExecute(String input){
+        return stackNumbers;
+    }
+    
 }
+
